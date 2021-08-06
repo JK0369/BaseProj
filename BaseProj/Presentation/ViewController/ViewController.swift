439c1e3c8aff9ec12b483026312415b9caf6b494
김종권 <jongkwon.kim@42dot.com>
2021-08-07 00:48:18 +0900

    Setting base project

---
+++ BaseProj/Presentation/ViewController/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  BaseProj
+//
+//  Created by 김종권 on 2021/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
